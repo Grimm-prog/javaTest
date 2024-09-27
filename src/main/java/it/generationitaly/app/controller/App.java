ff77a544cd0dfa207861a3a90f1f40f7b510919c
Rosa Piscitelli <piscitelli.rosa98@gmail.com>
2024-09-27 16:16:22 +0200

    mod syso

--- src/main/java/it/generationitaly/app/controller/App.java
+++ src/main/java/it/generationitaly/app/controller/App.java
@@ -3,7 +3,7 @@ package it.generationitaly.app.controller;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello Lorena");
 		metodo1();
 		metodo2();
 		System.out.println("Hello world!");
