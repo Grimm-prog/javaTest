31d182298cfb7902ee0a1c235b1e1c6064829c75
salva <salva@192.168.1.219>
2024-09-27 15:51:52 +0200

    add method 2

--- src/main/java/it/generationitaly/app/controller/App.java
+++ src/main/java/it/generationitaly/app/controller/App.java
@@ -5,10 +5,14 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		metodo1();
+		metodo2();
 	}
 	
 	private static void metodo1() {
 		System.out.println("metodo 1");
 	}
 
+	private static void metodo2() {
+		System.out.println("metodo 2");
+	}
 }
