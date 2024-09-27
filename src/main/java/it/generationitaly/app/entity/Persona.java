78af29c26d90574cbed8774a051c931a75c55b22
crist <crist@Cristian.homenet.telecomitalia.it>
2024-09-27 16:57:21 +0200

    Merge branch 'feature-lorena-cristian' of into feature-lorena-cristian

--- src/main/java/it/generationitaly/app/entity/Persona.java
+++ src/main/java/it/generationitaly/app/entity/Persona.java
@@ -2,30 +2,56 @@ package it.generationitaly.app.entity;
 
 public class Persona {
 
+	private int id;
 	private String nome;
 	private String cognome;
 	private String codiceFiscale;
-	
+	private Indirizzo indirizzo;
+
 	public String getNome() {
 		return nome;
 	}
+
 	@Override
 	public String toString() {
-		return "Persona [nome=" + nome + ", cognome=" + cognome + ", codiceFiscale=" + codiceFiscale + "]";
+		return "Persona [id=" + id + ", nome=" + nome + ", cognome=" + cognome + ", codiceFiscale=" + codiceFiscale
+				+ ", indirizzo=" + indirizzo + "]";
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCognome() {
 		return cognome;
 	}
+
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
+
 	public String getCodiceFiscale() {
 		return codiceFiscale;
 	}
+
 	public void setCodiceFiscale(String codiceFiscale) {
 		this.codiceFiscale = codiceFiscale;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Indirizzo getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(Indirizzo indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
 }
